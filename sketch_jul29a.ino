2f56d594bd47e73bafc9607e8e4116ce9036643f
yajpersonalproject <64474563+yajpersonalproject@users.noreply.github.com>
2022-08-21 10:45:30 +0800

    Add files via upload

---
+++ sketch_jul29a.ino
@@ -0,0 +1,42 @@
+#include <Servo.h>
+
+Servo claw, lifter, xRot;
+String entry;
+
+void setup()
+{
+  claw.attach(5);
+  lifter.attach(4);
+  xRot.attach(3);
+  Serial.begin(9600);
+  
+}
+
+void grablift(){
+  delay(2000);
+  claw.write(0);
+  delay(1000);
+  claw.write(180);
+  delay(1000);
+  lifter.write(0);
+  delay(1000);
+}
+
+void loop()
+{
+  xRot.write(90);
+  delay(1000);;
+  while(Serial.available()){
+    lifter.write(0);
+    delay(2000);
+    lifter.write(110);
+    entry = Serial.readString(); 
+    int cy = entry.toInt();
+    int moveangle = cy + (cy * 0.3);
+    delay(2000);
+    xRot.write(moveangle);
+    grablift();
+  }
+ }
+
+ 
